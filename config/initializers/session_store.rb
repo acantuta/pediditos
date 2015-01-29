734bb480104135c351e316e2f9ecf9342bec4ebe
Anthony Cantuta <anthony@cantuta.org>
2015-01-29 12:01:13 -0500

    Elasiza la session de usuario por un anio.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pediditos_session'
+Rails.application.config.session_store(:cookie_store,
+										key: '_pediditos_session',
+										expire_after: 60*60*24*365,
+										domain: :all)
